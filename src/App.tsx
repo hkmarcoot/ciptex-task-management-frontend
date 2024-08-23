6f2e0bf70d0f8f07a88a4bfa67e7fb959cabfa5d
hkmarcoot <hkmarcoot@gmail.com>
2024-08-23 18:09:22 +0100

    Initialise the react typescript tailwindcss vite project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-red-500">Hello world</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
